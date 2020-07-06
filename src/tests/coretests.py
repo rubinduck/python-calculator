50c7b4beb76ec793dd1a2bf520f87a99d87472cd
rubinduck <rubinduck@protonmail.com>
2020-07-06 08:21:36 +0300

    adapted test to previous commit changes

--- src/tests/coretests.py
+++ src/tests/coretests.py
@@ -2,48 +2,44 @@ import core
 
 def test_tokenizer():
 	get_tokens = core.get_tokens
-	assert get_tokens("1+2") == ["1","+","2"]
-	assert get_tokens("   1 +2") == ["1","+","2"]
-	assert get_tokens("   1 + 2   ") == ["1","+","2"]
-	assert get_tokens("   1.0 + 2   ") == ["1.0","+","2"]
-	assert get_tokens("1.0 + 2.0") == ["1.0","+","2.0"]
-	assert get_tokens("1.48 + 2.69843") == ["1.48","+","2.69843"]
 
+	assert get_tokens("   1 +2") == [1,"+",2]
+	assert get_tokens("   1 + 2   ") == [1,"+",2]
+	assert get_tokens("   1.0 + 2   ") == [1.0,"+",2]
+	assert get_tokens("1.0 + 2.0") == [1.0,"+",2.0]
+	assert get_tokens("1.48 + 2.69843") == [1.48,"+",2.69843]
 
-	assert get_tokens("1-2") == ["1","-","2"]
-	assert get_tokens("   1 -2") == ["1","-","2"]
-	assert get_tokens("   1 - 2   ") == ["1","-","2"]
-	assert get_tokens("   1.0 - 2   ") == ["1.0","-","2"]
-	assert get_tokens("1.0 - 2.0") == ["1.0","-","2.0"]
-	assert get_tokens("1.48 - 2.69843") == ["1.48","-","2.69843"]
+	assert get_tokens("   1 -2") == [1,"-",2]
+	assert get_tokens("   1 - 2   ") == [1,"-",2]
+	assert get_tokens("   1.0 - 2   ") == [1.0,"-",2]
+	assert get_tokens("1.0 - 2.0") == [1.0,"-",2.0]
+	assert get_tokens("1.48 - 2.69843") == [1.48,"-",2.69843]
 
-	assert get_tokens("1/2") == ["1","/","2"]
-	assert get_tokens("   1 /2") == ["1","/","2"]
-	assert get_tokens("   1 / 2   ") == ["1","/","2"]
-	assert get_tokens("   1.0 / 2   ") == ["1.0","/","2"]
-	assert get_tokens("1.0 / 2.0") == ["1.0","/","2.0"]
-	assert get_tokens("1.48 / 2.69843") == ["1.48","/","2.69843"]
+	assert get_tokens("   1 *2") == [1,"*",2]
+	assert get_tokens("   1 * 2   ") == [1,"*",2]
+	assert get_tokens("   1.0 * 2   ") == [1.0,"*",2]
+	assert get_tokens("1.0 * 2.0") == [1.0,"*",2.0]
+	assert get_tokens("1.48 * 2.69843") == [1.48,"*",2.69843]
 
-	assert get_tokens("1*2") == ["1","*","2"]
-	assert get_tokens("   1 *2") == ["1","*","2"]
-	assert get_tokens("   1 * 2   ") == ["1","*","2"]
-	assert get_tokens("   1.0 * 2   ") == ["1.0","*","2"]
-	assert get_tokens("1.0 * 2.0") == ["1.0","*","2.0"]
-	assert get_tokens("1.48 * 2.69843") == ["1.48","*","2.69843"]
+	assert get_tokens("   1 /2") == [1,"/",2]
+	assert get_tokens("   1 / 2   ") == [1,"/",2]
+	assert get_tokens("   1.0 / 2   ") == [1.0,"/",2]
+	assert get_tokens("1.0 / 2.0") == [1.0,"/",2.0]
+	assert get_tokens("1.48 / 2.69843") == [1.48,"/",2.69843]
 
-	assert get_tokens("5*(4 -12.3)") == ["5","*","(","4","-","12.3",")"]
-	assert get_tokens("(4*4)+4.5") == ['(','4','*','4',')','+','4.5']
+	assert get_tokens("5*(4 -12.3)") == [5,"*","(",4,"-",12.3,")"]
+	assert get_tokens("(4*4)+4.5") == ['(',4,'*',4,')','+',4.5]
 
 	print("tokenizer test succeeded")
 
 def test_rpn_convertor():
 	convert = core.convert_to_rpn
 
-	assert convert(["1","+","2"]) == [1,2,"+"]
-	assert convert(["15.37","+","2.8"]) == [15.37,2.8,"+"]
-	assert convert(["2","*","3","+","4"]) == [2,3,"*",4,"+"]
-	assert convert(["2","*","(","3","+","4",")"]) == [2,3,4,"+","*"]
-	assert convert(["5","/","(","35","*","8.7",")"]) == [5,35,8.7,"*","/"]
+	assert convert([1,"+",2]) == [1,2,"+"]
+	assert convert([15.37,"+",2.8]) == [15.37,2.8,"+"]
+	assert convert([2,"*",3,"+",4]) == [2,3,"*",4,"+"]
+	assert convert([2,"*","(",3,"+",4,")"]) == [2,3,4,"+","*"]
+	assert convert([5,"/","(",35,"*",8.7,")"]) == [5,35,8.7,"*","/"]
 
 	print("rpn-convertor test succeeded")
 
