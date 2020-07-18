8db50c15801d9cecd020b8bee018b86a08d6e293
rubinduck <rubinduck@protonmail.com>
2020-07-19 02:22:14 +0300

    Added test example for general-case control buttons grid

--- src/tests/guitests.py
+++ src/tests/guitests.py
@@ -13,7 +13,7 @@ def test_HistoryWidget():
 
 	class Main(QMainWindow):
 		"""
-		test class for HistoryWidget functionality
+		test class for HistoryWidget
 		"""
 		def __init__(self):
 			super().__init__()
@@ -37,7 +37,7 @@ def test_MainLineWidget():
 	MainLineWidget = gui.MainLineWidget
 	class Main(QMainWindow):
 		"""
-		test class for MainLineWidget functionality
+		test class for MainLineWidget
 		"""
 		def __init__(self):
 			super().__init__()
@@ -57,6 +57,33 @@ def test_MainLineWidget():
 
 	run_test_app(Main)
 
+def test_GeneralControlButtonsPanel():
+
+	GeneralControlButtonsPanel = gui.GeneralControlButtonsPanel
+	class Main(QMainWindow):
+		"""
+		test class for GeneralControlButtonsPanel
+		"""
+		def __init__(self):
+			super().__init__()
+			self.setGeometry(200,200,200,200)
+			self.init_ui()
+			self.show()
+
+		def init_ui(self):
+			self.content_container = QWidget(self)
+			self.setCentralWidget(self.content_container)
+
+			layout = QVBoxLayout(self.content_container)
+			self.content_container.setLayout(layout)
+
+			widget = GeneralControlButtonsPanel(2,self.content_container)
+			layout.addWidget(widget)
+
+			widget.set_buttons(["+","-","*"])
+
+	run_test_app(Main)
+
 
 def run_test_app(MainWindowClass):
 	app = QApplication([])
@@ -67,6 +94,7 @@ def run_test_app(MainWindowClass):
 def run_gui_tests():
 	test_HistoryWidget()
 	test_MainLineWidget()
+	test_GeneralControlButtonsPanel()
 
 if __name__ == "__main__":
 	run_gui_tests()
