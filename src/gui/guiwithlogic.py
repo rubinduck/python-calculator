5718c6369de16fcb3eadc7ce392b90e50416be92
rubinduck <rubinduck@protonmail.com>
2020-07-24 00:02:46 +0300

    Adde logic responsible for calling expression evaluating function from GUI and displaying results

--- src/gui/guiwithlogic.py
+++ src/gui/guiwithlogic.py
@@ -2,9 +2,10 @@
 Module conating unification of GUI and it's logic
 """
 
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt,pyqtSlot
 
 from .guiwidgets import CalculatorMainWindowGui
+from core import calculate_expression,IncorrectInputError
 
 class CalculatorMainWindow(CalculatorMainWindowGui):
 	"""
@@ -15,9 +16,10 @@ class CalculatorMainWindow(CalculatorMainWindowGui):
 		self.init_logic()
 
 	def init_logic(self):
-		self.set_input_focus()
+		self.setup_input_focus()
+		self.setup_calculation_logic()
 
-	def set_input_focus(self):
+	def setup_input_focus(self):
 		"""
 		Method creating behavior,where keyboard inputs goes to main line widget
 		of calculator
@@ -35,3 +37,21 @@ class CalculatorMainWindow(CalculatorMainWindowGui):
 			widget.setFocusProxy(main_line_widget)
 			widget.setFocusPolicy(Qt.NoFocus)
 
+	def setup_calculation_logic(self):
+		self._widgets["main_line"].returnPressed.connect(self.calculate)
+
+	@pyqtSlot()
+	def calculate(self,*args,**kargs):
+		"""
+		slot extracting expression from main line, evaluating it and
+		displaing results
+		"""
+		expression = self._widgets["main_line"].text()
+		try:
+			result = calculate_expression(expression)
+		except IncorrectInputError as ex:
+			# TODO error handling
+			print(ex)
+			return
+		self._widgets["history"].add_line(f"{expression}={result}")
+		self._widgets["main_line"].setText(str(result))
